37209a7990e25d1a5e35bb186afc74b6a8c4a093
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2023-04-11 18:30:38 +0530

    first commit

---
+++ schemas/comment.js
@@ -0,0 +1,48 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'message',
+      title: 'Message',
+      type: 'text',
+    }),
+    defineField({
+        name: 'user',
+        title: 'User',
+        type: 'reference',
+        to: {type: 'user'},
+        // initialValue: 'Rivoga',
+      }),
+    defineField({
+      name: 'post',
+      title: 'Post',
+      type: 'reference',
+      to: {type: 'post'},
+    }),
+    defineField({
+      name: 'parentId',
+      title: 'Parent ID',
+      type: 'reference',
+      to: {type: 'comment'},
+      initialValue: null,
+
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+  ],
+
+  // preview: {
+  //   select: {
+  //     title: 'user',
+  //     subtitle: 'message',
+  //   },
+  // },
+
+})
--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,8 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import comment from './comment'
+import user from './user'
 
-export const schemaTypes = [post, author, category, blockContent]
+// export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [comment, post, author, category, blockContent, user ]
